1e590d382c30a9221ce9a058e64c359c3e90d44e
Darius Foo <darius.foo.tw@gmail.com>
2024-05-04 12:30:50 +0800

    sed sucks

--- editor/examples.sh
+++ editor/examples.sh
@@ -4,7 +4,7 @@ files=(../examples/*.md)
 
 for file in "${files[@]}"; do
   contents="$(sed 's/"/\&quot;/g' < "$file")"
-  name=$(echo "$file" | sed -e 's/\.md//g' -e 's@../examples/@@g')
+  name=$(echo "$file" | sed -e 's/\.md//g' -e 's@../examples/@@g' | awk '{ print toupper(substr($0,1,1)) substr($0,2); }')
   transformed_contents="<option value=\"$name\" data-text=\"$contents\">$name</option>"
   output+="$transformed_contents"
 done
